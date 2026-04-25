c666120640b653e13af89eb199a941a6974631ff
Omidsheykhhassani <omidsheykhhasani2025@gmail.com>
2026-04-25 12:30:14 +0330

    V0.1.0 Planning the main page

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-hello world
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fitolife</title>
+</head>
+<body>
+  <!-- Navbar -->
+  <nav></nav>
+  <!-- Hero header -->
+  <header></header>
+  <!-- Features -->
+  <section></section>
+  <!-- Latest posts section -->
+  <section></section>
+  <!-- Latest post -->
+  <section></section>
+  <!-- Footer -->
+  <footer></footer>
+</body>
+</html>
