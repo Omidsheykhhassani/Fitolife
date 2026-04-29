64b038c6bd84711936a709417585dd9e4ae45196
Omidsheykhhassani <omidsheykhhasani2025@gmail.com>
2026-04-29 15:05:07 +0330

    V0.5.0 adding the navbar

--- functions.php
+++ functions.php
@@ -15,6 +15,6 @@ add_filter('show_admin_bar', '__return_false');
 function primary_button($text, $href)
 {
 ?>
-  <a href="<?php echo $href; ?>" class="text-whites-500 bg-primary-500 rounded-lg text-lg py-2 px-4 hover:bg-primary-700"><?php echo $text; ?></a>
+  <a href="<?php echo $href; ?>" class="bg-primary-500 rounded-lg text-lg py-2 px-4 hover:bg-primary-700"><?php echo $text; ?></a>
 <?php
 }
--- header.php
+++ header.php
@@ -10,4 +10,15 @@
 
 <body>
   <!-- Navbar -->
-  <nav></nav>
+  <nav class="fixed flex justify-center items-center p-4 z-1 w-full">
+    <div class="flex justify-between items-center w-full max-w-7xl rounded-full py-4 px-16 backdrop-blur-xs bg-white/5 border border-white/20 shadow-lg">
+      <ul class="flex flex-row gap-6">
+        <li><a href="#">خانه</a></li>
+        <li><a href="#">آرشیو مقالات</a></li>
+        <li><a href="#">یک لینک</a></li>
+      </ul>
+      <a href="/" class="flex justify-center items-center w-28 h-auto">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/text-logo-no-bg.png" alt="site logo">
+      </a>
+    </div>
+  </nav>
